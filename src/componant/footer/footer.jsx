3b6efcda59da859b12b3ddb8542a5681b1b634b3
Sh1n0u <florian.sune@gmail.com>
2023-08-24 15:14:35 +0200

    add footer

---
+++ src/componant/footer/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './footer.scss'
+function Footer() {
+    return <div className="footer">
+        <p className='text'>&copy; Florian Sune. Tous droits réservés</p>
+    </div>;
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom/client';
 import './style.css'
 
 import Header from './componant/header/header';
+import Footer from './componant/footer/footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Router>
             <Header />
+            <Footer />
         </Router>
     </React.StrictMode>
 );
