d3cefaef765302f28f235f904cc8ec718b987431
Sh1n0u <florian.sune@gmail.com>
2023-08-24 17:25:00 +0200

    add landing pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './style.css'
+import './style.css';
 
 import Header from './componant/header/header';
 import Footer from './componant/footer/footer';
+
+import Accueil from './pages/accueil/accueil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Router>
             <Header />
+            <Routes>
+                <Route exact path="/" element={<Accueil />} />
+            </Routes>
             <Footer />
         </Router>
     </React.StrictMode>
---
+++ src/pages/accueil/accueil.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './accueil.scss';
+
+function Accueil() {
+    return (
+        <div className="page-accueil">
+            <div className="block1">
+                <div className="title">
+                    <h1 className='text'>Console<span id="point-log">.log</span><span className='parenthesis'>&#40;</span><span className='typing-text'></span><span className='parenthesis'>&#41;</span></h1>
+                    <div>
+                        <h1 className='subtitle'>Développeur web</h1>
+                    </div>
+                </div>
+
+                <img src="./codeAccueil.jpg" alt="background de l'accueil" />
+            </div>
+            <div className="block2"></div>
+        </div>
+    );
+}
+
+export default Accueil;
