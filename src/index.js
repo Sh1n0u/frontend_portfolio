681e768e798385881a774e6aa29cea9eaefbc1cc
Sh1n0u <florian.sune@gmail.com>
2023-08-24 14:52:56 +0200

    add  component in index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import './style.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import Header from './componant/header/header';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Router>
             <Header />
         </Router>
     </React.StrictMode>
-)
+);
