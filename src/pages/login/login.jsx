7738c06e7d24d386ff2d9c0b859c768306dadc43
Sh1n0u <florian.sune@gmail.com>
2023-08-29 10:22:58 +0200

    add function login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,20 +1,44 @@
 import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import './login.scss';
+import axios from 'axios';
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
     };
+
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
     };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:4000/api/login', {
+                email: email,
+                password: password,
+            });
+            localStorage.setItem('token', response.data.token);
+            setIsLoggedIn(true);
+        } catch (error) {
+            console.error('Erreur lors de la requête:', error);
+        }
+    };
+
+    if (isLoggedIn) {
+        return <Navigate to="/admin" />;
+    }
+
     return (
         <div className="login-page">
             <p>Connexion</p>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="login-form">
                     <label htmlFor="email" />
                     <input
@@ -37,6 +61,7 @@ function Login() {
                         required
                     />
                 </div>
+                <button type="submit">Se connecter</button>
             </form>
         </div>
     );
