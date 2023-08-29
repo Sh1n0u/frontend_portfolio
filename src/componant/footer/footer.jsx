07b9c9db437e07518a38d2deb66b1ec20ef5fba9
Sh1n0u <florian.sune@gmail.com>
2023-08-29 10:21:33 +0200

    add function connexion

--- src/componant/footer/footer.jsx
+++ src/componant/footer/footer.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import './footer.scss'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './footer.scss';
+
 function Footer() {
-    return <div className="footer">
-        <p className='text'>&copy; Florian Sune. Tous droits réservés</p>
-    </div>;
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        setIsLoggedIn(!!token);
+    }, []);
+
+    return (
+        <div className="footer">
+            <p className="text">&copy; Florian Sune. Tous droits réservés</p>
+            {isLoggedIn ? (
+                <Link to="/admin">
+                    <img
+                        className="connexion-logo"
+                        src="./connexion-white.png"
+                        alt="logo connexion"
+                    />
+                </Link>
+            ) : (
+                <Link to="/login">
+                    <img
+                        className="connexion-logo"
+                        src="./connexion-black.png"
+                        alt="logo connexion"
+                    />
+                </Link>
+            )}
+        </div>
+    );
 }
 
 export default Footer;
