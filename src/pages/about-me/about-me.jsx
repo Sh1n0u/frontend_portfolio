4e1743af085e624e2cbd853b5fd5687fc040e855
Sh1n0u <florian.sune@gmail.com>
2023-08-30 09:58:37 +0200

    update aboutme

--- src/pages/about-me/about-me.jsx
+++ src/pages/about-me/about-me.jsx
@@ -7,10 +7,24 @@ function AboutMe() {
     }, []);
 
     return (
-        <div className='apropos-page'>
+        <div className="about-page">
             <h1>A propos</h1>
+            <div className="about-container">
+                <img src="./images/photo_Flo.jpg" alt="Photo Florian Sune" />
+                <div className="text-container">
+                    <p>
+                        Développeur Web Fullstack curieux et attiré par le FrontEnd au plus grand dépit de mon mentor, j'ai acquis une solide expérience en HTML, CSS, JavaScript et React pour concevoir
+                        des interfaces dynamiques.
+                    </p>
+                    <p>
+                        J'ai également développé des compétences en BackEnd avec Node.js et MongoDB pour construire des
+                        fonctionnalités robustes côté serveur.
+                    </p>
+                    <p>Toujours à la recherche de nouvelles compétences, je m'améliore au fil du temps.</p>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default AboutMe
+export default AboutMe;
