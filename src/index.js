0f796dfc0e9f652625ab542385f8f3a46e7f5797
Sh1n0u <florian.sune@gmail.com>
2023-08-30 09:11:51 +0200

    add experiences page

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Footer from './componant/footer/footer';
 
 import Accueil from './pages/accueil/accueil';
 import Competences from './pages/competences/competences';
+import Experiences from './pages/experiences/experiences';
 import Admin from './pages/admin/admin'
 import Login from './pages/login/login';
 import Error from './pages/error/error';
@@ -21,6 +22,7 @@ root.render(
                 <Route exact path="/" element={<Accueil />} />
                 <Route path='*' element={<Error />} />
                 <Route path='competence' element={<Competences />} />
+                <Route path='experience' element={<Experiences />} />
                 <Route path='login' element={<Login />} />
                 <Route path='admin' element={<Admin />} />
             </Routes>
