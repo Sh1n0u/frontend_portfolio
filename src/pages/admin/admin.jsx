4ef5f452cd98b23ef5452a71dc011dcbd0ba64b2
Sh1n0u <florian.sune@gmail.com>
2023-08-30 11:14:51 +0200

    fill admin page

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -3,12 +3,25 @@ import './admin.scss';
 
 function Competences() {
     useEffect(() => {
-        document.title = 'Panneau admin';
+        const token = localStorage.getItem('token');
+        if (!token) {
+            window.location.href = '/login';
+        } else {
+            document.title = 'Panneau admin';
+        }
     }, []);
 
     return (
-        <div className='admin-page'>
-
+        <div className="admin-page">
+            <h1>Panneau admin</h1>
+            <div className="list-container">
+                <div className="project-list">
+                    <p>Liste des projets</p>
+                </div>
+                <div className="message-list">
+                    <p>Liste des messages</p>
+                </div>
+            </div>
         </div>
     );
 }
