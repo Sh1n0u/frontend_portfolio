6c0966f3336fd8f3df3266b26ba09cd6beaabc80
Sh1n0u <florian.sune@gmail.com>
2023-09-04 16:24:52 +0200

    add project list

--- src/pages/experiences/experiences.jsx
+++ src/pages/experiences/experiences.jsx
@@ -1,16 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Box from '../../componant/box/box';
 import './experiences.scss';
 
 function Experiences() {
+    const [projects, setProjects] = useState([]);
+
     useEffect(() => {
         document.title = 'Expériences';
+        axios
+            .get('http://localhost:4000/api/projects')
+            .then((response) => {
+                setProjects(response.data);
+            })
+            .catch((error) => {
+                console.error('Erreur lors de la récupération des projets :', error);
+            });
     }, []);
 
     return (
         <div className="experience-page">
             <h1>Expériences</h1>
-            <div>
-
+            <div className="project-list">
+                {projects.map((project, index) => (
+                    <Box key={index} title={project.title} photoUrl={project.imageUrl} />
+                ))}
             </div>
         </div>
     );
