c93e4124de12d89e5802a7f60df5826244490dd4
Sh1n0u <florian.sune@gmail.com>
2023-09-05 12:03:36 +0200

    Fix error request PUT

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -6,21 +6,26 @@ import './modal.scss';
 function AdminPanel() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [projects, setProjects] = useState([]);
-
-    const [selectedProject, setSelectedProject] = useState(null);
     const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-    const [editedProject, setEditedProject] = useState({
-        id: null,
-        title: '',
-        description: '',
-    });
+    const [selectedProject, setSelectedProject] = useState(null);
+    const [addedProject, setAddedProject] = useState(null)
+    const [editedProject, setEditedProject] = useState(null);
 
     const openEditModal = (project) => {
         setSelectedProject(project);
-        setEditedProject(project);
         setIsEditModalOpen(true);
     };
 
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setIsEditModalOpen(false);
+    };
+
+    // Fonction POST
     const handleFormSubmit = async (event) => {
         event.preventDefault();
 
@@ -42,11 +47,13 @@ function AdminPanel() {
             const response = await axios.post('http://localhost:4000/api/projects', formData, config);
             console.log('Projet ajouté avec succès:', response.data);
             closeModal();
+            setAddedProject(response.data.project)
         } catch (error) {
             console.error("Erreur lors de l'ajout du projet:", error);
         }
     };
 
+    // Fonction GET
     useEffect(() => {
         const getProjects = async () => {
             try {
@@ -58,8 +65,20 @@ function AdminPanel() {
         };
 
         getProjects();
-    }, []);
+        if (addedProject) {
+            setProjects((prevProjects) => [addedProject, ...prevProjects]);
+        }
+        if (editedProject) {
+            setProjects((prevProjects) => {
+                const updatedProjects = prevProjects.map((project) =>
+                    project._id === editedProject._id ? editedProject : project
+                );
+                return updatedProjects;
+            });
+        }
+    }, [addedProject, editedProject]);
 
+    // Redirection user
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (!token) {
@@ -69,6 +88,7 @@ function AdminPanel() {
         }
     }, []);
 
+    // Fermeture modale par escape
     useEffect(() => {
         const handleKeyPress = (event) => {
             if (event.key === 'Escape') {
@@ -82,27 +102,22 @@ function AdminPanel() {
         };
     }, []);
 
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
-
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
-
-    const handleEditSubmit = async (event) => {
+    // Fonction PUT
+    const handleEditFormSubmit = async (event) => {
         event.preventDefault();
+
+        if (!selectedProject) {
+            console.error('Aucun projet sélectionné pour la modification.');
+            closeModal();
+            return;
+        }
 
         const form = event.target;
         const formData = new FormData();
 
         formData.append('title', form.title.value);
         formData.append('description', form.description.value);
-
-        // Vérifiez si un nouveau fichier a été sélectionné
-        if (form.newImage.files[0]) {
-            formData.append('newImage', form.newImage.files[0]);
-        }
+        formData.append('newImage', form.newImage.files[0]);
 
         const token = localStorage.getItem('token');
         const config = {
@@ -112,14 +127,18 @@ function AdminPanel() {
         };
 
         try {
+            console.log(selectedProject._id)
             const response = await axios.put(
-                `http://localhost:4000/api/projects/${editedProject.id}`,
+                `http://localhost:4000/api/projects/${selectedProject._id}`,
                 formData,
                 config
             );
             console.log('Projet modifié avec succès:', response.data);
-            setIsEditModalOpen(false);
+
+            closeModal();
+            setEditedProject(response.data.project);
         } catch (error) {
+            console.log(selectedProject._id)
             console.error('Erreur lors de la modification du projet:', error);
         }
     };
@@ -131,13 +150,45 @@ function AdminPanel() {
                 <div className="project-list">
                     <h2>Liste des projets</h2>
                     <ul>
-                        {projects.map((project) => (
-                            <li key={project.id} onClick={() => openEditModal(project)}>
+                        {projects.map((project, index) => (
+                            <li key={index}>
                                 {project.title}
+                                <button onClick={() => openEditModal(project)}>Modifier</button>
+                                
                             </li>
                         ))}
                     </ul>
                 </div>
+                {/* Modale modifier */}
+                {isEditModalOpen && selectedProject && (
+                    <div className="modal">
+                        <div className="modal-container">
+                            <span className="close" onClick={closeModal}>
+                                &times;
+                            </span>
+                            <div className="form-container">
+                                <h2>Modifier le projet</h2>
+                                <form onSubmit={handleEditFormSubmit}>
+                                    <label htmlFor="title"></label>
+                                    <input
+                                        type="text"
+                                        name="title"
+                                        placeholder="Titre du projet"
+                                        defaultValue={selectedProject.title}
+                                    />
+
+                                    <label htmlFor="newImage"></label>
+                                    <input id="newImage" type="file" name="newImage" />
+
+                                    <label htmlFor="description"></label>
+                                    <textarea name="description" defaultValue={selectedProject.description}></textarea>
+
+                                    <button type="submit">Modifier</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                )}
                 <div className="message-list">
                     <h2>Liste des messages</h2>
                 </div>
@@ -160,49 +211,6 @@ function AdminPanel() {
                                     <textarea name="description"></textarea>
 
                                     <button type="submit">Ajouter</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                )}
-                {isEditModalOpen && (
-                    <div className="modal">
-                        <div className="modal-container">
-                            <span className="close" onClick={() => setIsEditModalOpen(false)}>
-                                &times;
-                            </span>
-                            <div className="form-container">
-                                <h2>Modifier le projet</h2>
-                                <form onSubmit={handleEditSubmit}>
-                                    <label htmlFor="title"></label>
-                                    <input
-                                        type="text"
-                                        name="title"
-                                        placeholder="Titre du projet"
-                                        value={editedProject.title}
-                                        onChange={(e) =>
-                                            setEditedProject({
-                                                ...editedProject,
-                                                title: e.target.value,
-                                            })
-                                        }
-                                    />
-                                    <label htmlFor="newImage"></label>
-                                    <input id="newImage" type="file" name="newImage" />
-
-                                    <label htmlFor="description"></label>
-                                    <textarea
-                                        name="description"
-                                        value={editedProject.description}
-                                        onChange={(e) =>
-                                            setEditedProject({
-                                                ...editedProject,
-                                                description: e.target.value,
-                                            })
-                                        }
-                                    ></textarea>
-
-                                    <button type="submit">Modifier</button>
                                 </form>
                             </div>
                         </div>
