1d0c566ae8e64b7789445e97e7a22ff71fa295f8
Sh1n0u <florian.sune@gmail.com>
2023-09-05 12:42:10 +0200

    add contact route

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import Accueil from './pages/accueil/accueil';
 import Competences from './pages/competences/competences';
 import Experiences from './pages/experiences/experiences';
 import AboutMe from './pages/about-me/about-me';
+import Contact from './pages/contact/contact';
 import Admin from './pages/admin/admin'
 import Login from './pages/login/login';
 import Error from './pages/error/error';
@@ -25,6 +26,7 @@ root.render(
                 <Route path='competence' element={<Competences />} />
                 <Route path='experience' element={<Experiences />} />
                 <Route path='apropos' element={<AboutMe />} />
+                <Route path='contact' element={<Contact />} />
                 <Route path='login' element={<Login />} />
                 <Route path='admin' element={<Admin />} />
             </Routes>
