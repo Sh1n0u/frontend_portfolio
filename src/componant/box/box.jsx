451ecf77a667efbe390de1279265c855b5c90d7e
Sh1n0u <florian.sune@gmail.com>
2023-09-08 17:08:27 +0200

    add Onclick prop

--- src/componant/box/box.jsx
+++ src/componant/box/box.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './box.scss';
 
-const Box = ({ title, photoUrl, description }) => {
+const Box = ({ title, photoUrl, description, onClick }) => {
     return (
-        <div className="box">
+        <div className="box" onClick={onClick}>
             <h2>{title}</h2>
             <img src={photoUrl} alt="" />
             <p>{description}</p>
@@ -16,6 +16,7 @@ Box.propTypes = {
     title: PropTypes.string.isRequired,
     photoUrl: PropTypes.string.isRequired,
     description: PropTypes.string,
+    onClick: PropTypes.func,
 };
 
 export default Box;
