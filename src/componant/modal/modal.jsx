7585f97a6c83737cdfae3a4c741cead0dfd173c4
Sh1n0u <florian.sune@gmail.com>
2023-09-14 17:07:47 +0200

    edit modal generic componant

--- src/componant/modal/modal.jsx
+++ src/componant/modal/modal.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './modal.scss';
 
 function Modal({ isOpen, onClose, children }) {
-  if (!isOpen) return null;
+    useEffect(() => {
+        const handleEscKeyPress = (event) => {
+            if (event.key === 'Escape' && isOpen) {
+                onClose();
+            }
+        };
 
-  return (
-    <div className="modal">
-      <div className="modal-container">
-        <span className="close" onClick={onClose}>
-          &times;
-        </span>
-        <div className="modal-content">{children}</div>
-      </div>
-    </div>
-  );
+        // Ajouter le gestionnaire d'événements lors de l'ouverture de la modale
+        if (isOpen) {
+            window.addEventListener('keydown', handleEscKeyPress);
+        }
+
+        // Supprimer le gestionnaire d'événements lors de la fermeture de la modale ou du démontage du composant
+        return () => {
+            window.removeEventListener('keydown', handleEscKeyPress);
+        };
+    }, []);
+
+    return (
+        <div className={`modal ${isOpen ? 'modal-open' : ''}`}>
+            <div className="modal-container">
+                <span className="close" onClick={onClose}>
+                    &times;
+                </span>
+                <div className="modal-content">{children}</div>
+            </div>
+        </div>
+    );
 }
 
 export default Modal;
